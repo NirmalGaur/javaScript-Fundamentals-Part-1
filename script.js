f444e65bfb0d71016017e8965d6fd9619d2e530a
Nirmal Gaur <nirmalgaur814@gmail.com>
2021-09-08 17:07:31 +0530

    Coding Challenge 3

--- script.js
+++ script.js
@@ -40,3 +40,58 @@ if (markBMI > johnBMI) {
 else {
     console.log(`John's BMI (${johnBMI}) is higher than Marks's (${markBMI})`);
 }
+
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+// score, as well as multiple else-if blocks 
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+let dolpscore1 = 96, dolpscore2 = 108, dolpscore3 = 89;
+let koalscore1 = 88, koalscore2 = 91, koalscore3 = 110;
+if ((dolpscore1 + dolpscore2 + dolpscore3) / 3 > (koalscore1 + koalscore2 + koalscore3) / 3)
+    console.log("team Dolphins wins!");
+else if ((dolpscore1 + dolpscore2 + dolpscore3) / 3 < (koalscore1 + koalscore2 + koalscore3) / 3)
+    console.log("team Koalas wins!");
+else
+    console.log("there is a draw");
+//Bonus 1:
+dolpscore1 = 97, dolpscore2 = 112, dolpscore3 = 101;
+koalscore1 = 109, koalscore2 = 95, koalscore3 = 123;
+let avgscoreDolp = (dolpscore1 + dolpscore2 + dolpscore3) / 3;
+let avgscoreKoal = (koalscore1 + koalscore2 + koalscore3) / 3;
+if ((avgscoreDolp > avgscoreKoal) && (avgscoreDolp >= 100))
+    console.log("team Dolphins wins!");
+else if ((avgscoreDolp < avgscoreKoal) && (avgscoreKoal >= 100))
+    console.log("team Koalas wins!");
+else
+    console.log("there is a draw");
+//Bonus 2:
+dolpscore1 = 97, dolpscore2 = 112, dolpscore3 = 101;
+koalscore1 = 109, koalscore2 = 95, koalscore3 = 106;
+avgscoreDolp = (dolpscore1 + dolpscore2 + dolpscore3) / 3;
+avgscoreKoal = (koalscore1 + koalscore2 + koalscore3) / 3;
+if ((avgscoreDolp > avgscoreKoal) && (avgscoreDolp >= 100))
+    console.log("team Dolphins wins!");
+else if ((avgscoreDolp < avgscoreKoal) && (avgscoreKoal >= 100))
+    console.log("team Koalas wins!");
+else {
+    if (avgscoreDolp >= 100)
+        console.log("there is a draw");
+    else
+        console.log("no one wins");
+}
